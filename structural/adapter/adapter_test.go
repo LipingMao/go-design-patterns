bf4aa4ae8e7275837d227dc27a22d93dbc8227e8
Liping Mao <limao455@163.com>
2019-10-24 16:40:36 +0800

    structural: adapter

--- structural/adapter/adapter_test.go
+++ structural/adapter/adapter_test.go
@@ -9,7 +9,7 @@ func TestPrinterAdapter_PrintStored(t *testing.T) {
 	adapter := PrinterAdapter{OldPrinter: &MyLagencyPrinter{}, Msg: "Hello World"}
 	old := adapter.OldPrinter.Print("Hello World")
 	if !strings.Contains(old, "Lagency") {
-		t.Errorf("OldPrinter Error : %v", old)
+		t.Errorf("Lagency Error : %v", old)
 	}
 	new := adapter.PrintStored()
 	if !strings.Contains(new, "Modern") {
